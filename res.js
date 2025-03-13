faedfd5f2f0ee1ad397f10d949f5bed9679ca977
geekyharry <singhaal.mail@gmail.com>
2025-03-13 20:29:12 +0530

    New-Rel Sep

--- res.js
+++ res.js
@@ -1,4 +1,4 @@
-//Booking Station
+//Seat Selection
 
 let seats = document.querySelector(".all-seats");
 for(let i=0; i<59;i++){
@@ -33,41 +33,3 @@ tickets.forEach((ticket)=>{
     document.querySelector(".count").innerHTML = count;
   })
 })
-
-//Slider
-
-const left = document.querySelector('.lf');
-const right = document.querySelector('.rg');
-const slidr = document.querySelector('.slider');
-const imag = document.querySelectorAll('.image');
-
-let slideNumber = 1 
-const length = imag.length
-
-const nxtsld = ()=>{
-  slidr.style.transform = `translateX(-${slideNumber*1100}px)`;
-  slideNumber++;
-}
-
-const presld = ()=>{
-  slidr.style.transform = `translateX(-${(slideNumber-2)*1100}px)`;
-  slideNumber--;
-}
-
-const firstsld = ()=>{
-  slidr.style.transform = `translateX(-0px)`;
-  slideNumber = 1;
-}
-
-const lstsld = ()=>{
-  slidr.style.transform = `translateX(-${(length-1)*1100}px)`;
-  slideNumber = length;
-}
-
-right.addEventListener('click', ()=> {
-  slideNumber<length ? nxtsld() : firstsld();
-});
-
-left.addEventListener('click', ()=> {
-  slideNumber > 1 ? presld() : lstsld();
-});
